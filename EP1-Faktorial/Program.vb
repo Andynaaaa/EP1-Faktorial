b7fba5c7ce36998ff1e0ebbdb57d6521b4b3e8fb
Andrea Tannenberger <Tannenberger.Andrea@stud.vassboskovice.cz>
2024-05-13 13:44:13 +0200

    po dokončení výpočtu opět zadat další výpočet a´

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -4,31 +4,24 @@ Module Program
     Sub Main(args As String())
         Dim cislo As Integer
         Dim soucet As Integer = 0
+        Do
+            Console.Write("Zadej číslo: ")
+            cislo = Console.ReadLine
+            If cislo <= 0 Then
+                Console.Write("Chybný vstup. Zadejte prosím celé číslo.")
+                End
+            End If
 
-        Console.Write("Zadej číslo: ")
-        cislo = Console.ReadLine
-        If cislo <= 0 Then
-            Console.Write("Chybný vstup. Zadejte prosím celé číslo.")
-            End
-        End If
+            If cislo > 10 Then
+                Console.Write("Zadali jste hodnotu vyšší než 10. Prosím zadejte nižší hodnotu.")
+                End
+            End If
 
-        If cislo > 10 Then
-            Console.Write("Zadali jste hodnotu vyšší než 10. Prosím zadejte nižší hodnotu.")
-            End
-        End If
-
-        If cislo = 1 > 0 Then
-            Console.Write("Záporné hodnoty nejsou povoleny. Zadejte prosím kladné celé číslo.")
-            End
-        End If
-
-
-
-
-
-
-
-
+            If cislo = 1 > 0 Then
+                Console.Write("Záporné hodnoty nejsou povoleny. Zadejte prosím kladné celé číslo.")
+                End
+            End If
+        Loop
 
         For i = 1 To cislo
             soucet = soucet + i
